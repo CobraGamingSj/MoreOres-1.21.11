1a381a1f37249b1a250896d054f19f96bfd99309
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2025-12-19 16:14:25 +0530

    Initial commit

---
+++ src/main/java/net/cobra/moreores/client/render/block/entity/GemPurifierBlockEntityRenderState.java
@@ -0,0 +1,23 @@
+package net.cobra.moreores.client.render.block.entity;
+
+import net.cobra.moreores.block.entity.GemPurifierBlockEntity;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
+import net.minecraft.client.render.item.ItemRenderState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class GemPurifierBlockEntityRenderState extends BlockEntityRenderState {
+
+    public GemPurifierBlockEntity entity;
+    public World entityWorld;
+    public BlockPos lightPos;
+
+    final ItemRenderState inputItemRenderState = new ItemRenderState();
+    final ItemRenderState energyItemRenderState = new ItemRenderState();
+    final ItemRenderState resultItemRenderState = new ItemRenderState();
+
+    public void setEntity(GemPurifierBlockEntity entity) {
+        this.entity = entity;
+    }
+}
